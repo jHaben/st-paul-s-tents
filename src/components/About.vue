7ae65e377abdaf6a52004b7404970213393245b8
jack <jrhaben@gmail.com>
2023-06-27 11:16:44 -0500

    about update

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,8 +6,8 @@
             <v-col />
             <v-col class="justify-center align-center about" cols="4">
 
-                Hello and Welcome to St. Paul's Tents and More where we provide the best in your selection of party rentals
-                and furniture, from picnic tables to large beautiful tents for your party. Our products for purchase are
+                Hello and Welcome to St. Paul's Tents and More where we provide party rentals
+                and outdoor furniture, from picnic tables to large beautiful tents for your party. Our products for purchase are
                 handcrafted here in St. Paul, MN. Our rental program comes with help setting up and taking down so you don't
                 have to worry and can focus on your party. Sends us message or give us call today!
             </v-col>
